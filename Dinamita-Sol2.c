303154ab962fb9942c5a3e442d6e1e4f28ecf280
JoseBaezaP <josedelarosabass@gmail.com>
2018-01-26 13:04:22 -0600

    nuevo

--- Dinamita-Sol2.c
+++ Dinamita-Sol2.c
@@ -1,30 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 /*Autor: José de la Rosa Baeza Pérez
 
 Intoduccion: El programa va recibir un numero,
 ya teniendo esto se analizara con un ciclo for
-para luego determinar cuantos digitos tiene ese numero*/
-
-int main(int argc, char *argv[]) {
-	//Se definen las variables
-	int numero,i=1,salida,n=1; //variables
+para luego determinar cuantos digitos tiene ese numeron*/
+int main()
+{
+	/*Se definen las variables*/
+	int numero,i=0,salida; /*variables
 	
-	//Entrada
+	Entrada*/
 	printf("Ingrese el numero deseado \n");
-	scanf("%i",&numero);
+	scanf("%d",&numero);
 	
-	//Procedimiento
+	/*Procedimiento*/
 	do
 	{
-		numero/=10;//Aqui se va dividiendo el numero para que la condicion se cumpla y asi hacer que este llegue a 0
-		i++;//Este contados nos ayuda a saber cuandos digitos tiene el numero
+		numero=numero/10;/*Aqui se va dividiendo el numero para que la condicion se cumpla y asi hacer que este llegue a 0*/
+		i=i+1;/*Este contados nos ayuda a saber cuandos digitos tiene el numero*/
 	}
-	while(numero==0);//La condicion para cerrar el ciclo
+	while(numero!=0);/*La condicion para cerrar el ciclo*/
 	salida=i;
 	
-	//Salida: En este momento se imprime la variable la cual indica la cantidad de digitos que tiene dicho numero
+	/*Salida: En este momento se imprime la variable la cual indica la cantidad de digitos que tiene dicho numero*/
 	printf("La cantidad de digitos que tiene este numero es %d",salida);
-	
-	return 0;
+	return(0);
 }
-
